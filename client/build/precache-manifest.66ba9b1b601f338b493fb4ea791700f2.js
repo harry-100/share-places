e83daff0c57c06a7c04095931fabc075e7a4461b
Harry Shah <harvinder.shah@gmail.com>
2021-12-31 13:44:21 -0500

    heroku deploy

---
+++ client/build/precache-manifest.66ba9b1b601f338b493fb4ea791700f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b19ae61e313df159483a17130064720e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f90cb2fe53e53cee613",
+    "url": "/static/css/main.cd63b97c.chunk.css"
+  },
+  {
+    "revision": "8ce21ee57ebda2a10800",
+    "url": "/static/js/2.07144096.chunk.js"
+  },
+  {
+    "revision": "3f90cb2fe53e53cee613",
+    "url": "/static/js/main.d6da4fe7.chunk.js"
+  },
+  {
+    "revision": "fa2580749a3fec3e1ba3",
+    "url": "/static/js/runtime-main.51c80e69.js"
+  }
+]);
