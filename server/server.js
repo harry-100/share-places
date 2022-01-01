564b4cc738c8f5318d238e7dc0b44d058313a9b9
Harry Shah <harvinder.shah@gmail.com>
2022-01-01 08:59:35 -0500

    server.js updated

--- server/server.js
+++ server/server.js
@@ -1,49 +1,51 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-require("dotenv").config();
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
-const placesRoutes = require("./routes/places-routes");
-const usersRoutes = require("./routes/users-routes");
-
-const HttpError = require("./models/http-error");
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3001;
+
+const placesRoutes = require('./routes/places-routes');
+const usersRoutes = require('./routes/users-routes');
+const HttpError = require('./models/http-error');
+
 const app = express();
 
 app.use(bodyParser.json());
+
 app.use('/uploads/images', express.static(path.join('uploads', 'images')));
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   );
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+
   next();
 });
 
-app.use("/api/places", placesRoutes);
-app.use("/api/users", usersRoutes);
+app.use('/api/places', placesRoutes);
+app.use('/api/users', usersRoutes);
 
 app.use((req, res, next) => {
-  const error = new HttpError("Could not find this route.", 404);
+  const error = new HttpError('Could not find this route.', 404);
   throw error;
 });
 
 app.use((error, req, res, next) => {
-  if (req.file){
+  if (req.file) {
     fs.unlink(req.file.path, err => {
-      console.log(err)
-    })
+      console.log(err);
+    });
   }
   if (res.headerSent) {
     return next(error);
   }
   res.status(error.code || 500);
-  res.json({ message: error.message || "An unknown error occurred!" });
+  res.json({ message: error.message || 'An unknown error occurred!' });
 });
 
 mongoose
@@ -53,6 +55,6 @@ mongoose
   .then(() => {
     app.listen(PORT);
   })
-  .catch((err) => {
+  .catch(err => {
     console.log(err);
   });
