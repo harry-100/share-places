1b90637e61fe728274b76c2fa71c8b3ffc67a84c
Harry Shah <harvinder.shah@gmail.com>
2022-01-01 09:55:10 -0500

    image upload updated

--- server/middleware/file-upload.js
+++ server/middleware/file-upload.js
@@ -1,29 +1,28 @@
 const multer = require('multer');
-const { v4: uuid } = require("uuid");
-
+const uuid = require('uuid/v1');
 
 const MIME_TYPE_MAP = {
-    'image/png': 'png',
-    'image/jpeg': 'jpeg',
-    'image/jpg': 'jpg'
+  'image/png': 'png',
+  'image/jpeg': 'jpeg',
+  'image/jpg': 'jpg'
 };
 
 const fileUpload = multer({
-limit: 500000,
-storage: multer.diskStorage({
+  limits: 500000,
+  storage: multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'uploads/images');
+      cb(null, 'uploads/images');
     },
-    filename:(req, file, cb) => {
-        const ext = MIME_TYPE_MAP[file.mimetype];
-        cb(null, uuid() + '.' + ext);
+    filename: (req, file, cb) => {
+      const ext = MIME_TYPE_MAP[file.mimetype];
+      cb(null, uuid() + '.' + ext);
     }
-}),
-fileFilter:(req, file, cb) => {
-    const isValid = !!MIME_TYPE_MAP[file.mimetype];//double bang(!!) converts undefined to false
-    let error = isValid ? null : new Error('Invalid mime type');
-    cb(error, isValid); 
-}
+  }),
+  fileFilter: (req, file, cb) => {
+    const isValid = !!MIME_TYPE_MAP[file.mimetype];
+    let error = isValid ? null : new Error('Invalid mime type!');
+    cb(error, isValid);
+  }
 });
 
 module.exports = fileUpload;
